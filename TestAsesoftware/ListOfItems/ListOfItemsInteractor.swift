de267a6ebfd337d773bc6db4aabfb673d3c06017
Michael Rodriguez <miarodriguezur@unal.edu.co>
2024-02-02 22:32:19 -0500

    VIPER classes are created for the ListOfItems module

---
+++ TestAsesoftware/ListOfItems/ListOfItemsInteractor.swift
@@ -0,0 +1,8 @@
+//
+//  ListOfItemsInteractor.swift
+//  TestAsesoftware
+//
+//  Created by Michael Alexander Rodriguez Urbina on 2/02/24.
+//
+
+import Foundation
---
+++ TestAsesoftware/ListOfItems/ListOfItemsRouter.swift
@@ -0,0 +1,8 @@
+//
+//  ListOfItemsRouter.swift
+//  TestAsesoftware
+//
+//  Created by Michael Alexander Rodriguez Urbina on 2/02/24.
+//
+
+import Foundation

187300b428fbdca3ec2c09b404ff742bdb644957
Michael Rodriguez <miarodriguezur@unal.edu.co>
2024-02-03 11:40:47 -0500

    The HTTPClient protocol has been moved to its own file

---
+++ TestAsesoftware/Networking/HTTPClient.swift
@@ -0,0 +1,17 @@
+//
+//  HTTPClient.swift
+//  TestAsesoftware
+//
+//  Created by Michael Alexander Rodriguez Urbina on 3/02/24.
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
